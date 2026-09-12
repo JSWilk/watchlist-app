53bffc08b0da4bdc6ad6c57c6528f1b798b8a5fb
Justin Wilk <justinstefan.wilk@gmail.com>
2026-09-12 13:28:30 +0200

    Add DB schema for title/lists/list_entries

--- DATABASE_SCHEMA.sql
+++ DATABASE_SCHEMA.sql
@@ -1,1 +1,19 @@
+CREATE TABLE title(
+    id                    SERIAL        PRIMARY KEY,
+    name                  VARCHAR(100)  NOT NULL,
+    category              VARCHAR(50)   NOT NULL,
+    length_minutes        INT,
+    provider              VARCHAR(50),
+    watched               BOOLEAN       DEFAULT false
+);
 
+CREATE TABLE lists(
+    id     SERIAL         PRIMARY KEY,
+    name   VARCHAR(100)   NOT NULL
+);
+
+CREATE TABLE list_entries(
+    list_id               INT REFERENCES lists(id) ON DELETE CASCADE,
+    title_id              INT REFERENCES title(id) ON DELETE CASCADE,
+    PRIMARY KEY (list_id, title_id)
+);
